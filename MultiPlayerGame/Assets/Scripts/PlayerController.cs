d8e9116c900ef72b976d668b01a331501e234e96
Rishav Bhagat <rishavb123@gmail.com>
2020-05-01 20:13:33 -0400

    Basic setup for prefabs

---
+++ MultiPlayerGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public bool isLocalPlayer;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
